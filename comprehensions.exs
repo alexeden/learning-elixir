86a8e7adb5e0cb7f98933a1dd3e8352ac9dc7a4a
alexeden <AlexanderEden91@gmail.com>
2016-10-10 02:46:56 -0500

    More practice implementations:

--- comprehensions.exs
+++ comprehensions.exs
@@ -1,6 +1,8 @@
 defmodule Comprehensions do
   # comprehension syntax:
   # for generator or filter... [, into: value ], do: expression
-  
+  def span(from, to) do
+    for x <- from..to, do: x
+  end
 
 end
--- enums.exs
+++ enums.exs
@@ -1,3 +1,21 @@
 defmodule Enums do
 
+  # usage: Enums.all? [1, 2, 3, 4], &(&1 < 4)
+  def all?([], _pred), do: true
+  def all?([head | tail], pred), do: pred.(head) && all?(tail, pred)
+
+  # usage: Enums.filter [1, 2, 3, 4], &(&1 < 4)
+  def filter([], _pred), do: []
+  def filter([head | tail], pred) do
+    if pred.(head) == true do
+      [head | filter(tail, pred)]
+    else
+      filter(tail, pred)
+    end
+  end
+
+  def take(_list, 0), do: []
+  def take([], _n), do: []   
+  def take([head | tail], n) when n > 0, do: [head | take(tail, n - 1)]
+
 end
